84c2ebb82e30943fef7d947cc677bc5f4e952550
Michael Pfammatter <mpfammatter@Michaels-iMac.local>
2020-01-01 08:29:22 -0600

    Add an emacs layout for endor

--- hass-api.el
+++ hass-api.el
@@ -241,12 +241,19 @@
   (message "%s" hass-api/url))
 (spacemacs/set-leader-keys "ahh" 'mjp/browse-url-hassio)
 
-(spacemacs|define-custom-layout "eulerremote"
+(spacemacs|define-custom-layout "euler"
   :binding "e"
   :body
   (let ((sshConfig (if (= 0 (call-process "ping" nil nil nil "192.168.40.139" "-c" "1" "-W" "1")) "eulerlocal" "eulerremote")))
     (message "connecting to %s" sshConfig)
     (find-file (concat "/scp:" sshConfig ":/config/"))
     (magit-status)))
+
+(spacemacs|define-custom-layout "endor"
+  :binding "n"
+  :body
+  (let ((sshConfig (if (= 0 (call-process "ping" nil nil nil "192.168.40.159" "-c" "1" "-W" "1")) "endorlocal" "endoremote")))
+    (message "connecting to %s" sshConfig)
+    (find-file (concat "/scp:" sshConfig ":/homeassistant/"))))
 
 (provide 'hass-api)
